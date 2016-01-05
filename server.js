8ed3bf4515c3205df8fa9a4cf4f15a19b068590d
aequanimitas <htavenido@gmail.com>
2016-01-05 16:59:10 +0800

    layout first before going to react

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./public'));
-app.use(express.static('./node_modules/bootstrap/dist'));
-app.use(express.static('./node_modules/font-awesome'));
+app.use(express.static('./node_modules/milligram/dist'));
 
 app.listen(process.env.PORT || 3000);
