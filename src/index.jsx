5e3c2c10eaf654cef7ab873df10121c81f943132
aequanimitas <htavenido@gmail.com>
2016-01-07 18:32:36 +0800

    always align the opening tag with the return kw

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <form>
-           <label htmlFor="search-field"> Search </label>
-           <input type="text" id="search-field" placeholder="procrastination, laziness" />
-           <button type="submit">Search!</button>
-        </form>
-        <section> hot loader? </section>
-      </div>
-      ) 
-  }
-}
+import App from './containers/App';
 
 ReactDOM.render(
   <App /> , 
