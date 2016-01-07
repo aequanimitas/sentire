b0af1a9284054ad21b4beae470f313250422d98f
aequanimitas <htavenido@gmail.com>
2016-01-08 03:42:49 +0800

    pass a temp quote to component

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export default React.createClass({
+  getQuote: function() {
+    return this.props.quote || [];
+  },
+
   render: function() {
-    return <div className="quote-container"> <span className="quote-author"></span></div>;
+    return <div className="quote-container"> 
+        <span className="quote-author">{this.props.quote.author}</span>
+    </div>;
   }
 });
--- test/components/quote_spec.jsx
+++ test/components/quote_spec.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react/lib/ReactTestUtils';
+import {Map, List} from 'immutable'
 import Quote from '../../src/components/Quote';
 import { expect } from 'chai';
 
 describe('Quote box', () => {
   it('should have an author span', () => {
-    const component = ReactTestUtils.renderIntoDocument( <Quote /> );
+    const quote = Map({
+        quote: 'With regard to whatever objects give you delight, are useful, or are ' +
+               'deeply loved, remember to tell yourself of what general nature they ' +
+               'are, beginning from the most insignificant things. If, for example, ' +
+               'you are fond of a specific ceramic cup, remind yourself that it is ' +
+               'only ceramic cups in general of which you are fond. Then, if it ' +
+               'breaks, you will not be disturbed. If you kiss your child, or your ' +
+               'wife, say that you only kiss things which are human, and thus you' +
+               'will not be disturbed if either of them dies.',
+         author: 'epictetus',
+         tags: List.of('attachment')
+      });
+
+    const component = ReactTestUtils.renderIntoDocument(
+        <Quote quote={quote.toObject()}/>
+    );
     const quoteAuthor = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'span');
     expect(quoteAuthor.length).to.equal(1);
-    expect(quoteAuthor[0].classList[0]).to.equal('quote-author');
+    console.log(quoteAuthor[0]);
+    expect(quoteAuthor[0].textContent).to.equal('epictetus');
   });
 })
