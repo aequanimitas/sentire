8bf4db5b4d654ddd1600a4bd330692a0b906cf04
aequanimitas <htavenido@gmail.com>
2016-01-11 16:18:36 +0800

    unify into single webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+var entry = [ './src/index.jsx' ];
+var plugins = [];
+var isDev = new webpack.DefinePlugin({
+  __ISDEV__ : JSON.stringify(JSON.parse(process.env.debug || 'false'))
+});
+
+if (process.env.DEBUG) {
+  // add dev entries
+  entry.unshift('file?name=index.html!jade-html!./views/index.jade');
+  entry.unshift('webpack/hot/only-dev-server');
+  entry.unshift('webpack-dev-server/client?http://localhost:10000');
+
+  // add plugins
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+  plugins.push(isDev);
+}
+
+module.exports = {
+  entry: entry,
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/, loader: 'style-loader!css-loader!'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 10000
+  },
+  plugins: plugins
+};
