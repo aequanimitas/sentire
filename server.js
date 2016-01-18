828c0727c51f783f302ec9f12564c6040976972c
aequanimitas <htavenido@gmail.com>
2016-01-19 02:54:47 +0800

    remove extra space

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var webpack = require('webpack');
 var config = require('./webpack.config');
-
 var app = express();
 var compiler = webpack(config);
 
