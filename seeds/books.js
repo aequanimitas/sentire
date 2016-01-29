6415ed641c9d48e0f00a024cacb70c2f6000e256
aequanimitas <htavenido@gmail.com>
2016-01-30 05:28:46 +0800

    insert one book

---
+++ seeds/books.js
@@ -0,0 +1,10 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('books').del(), 
+
+    // Inserts seed entries
+    knex('books').insert({name: 'enchiridion', author_id:1}) 
+  );
+};
