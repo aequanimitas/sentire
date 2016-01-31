c776d5eaf140b9fe9e67c6a7ee3ce43acbf7422a
aequanimitas <htavenido@gmail.com>
2016-02-01 07:48:29 +0800

    toggle

--- client/components/Entry.js
+++ client/components/Entry.js
@@ -1,26 +1,31 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 class Entry extends Component {
   constructor(props) {
     super(props);
-    this.state = { disabled: false };
+    this.state = { more: false };
+  }
+
+  seeMore() {
+    this.setState({ more: this.state.more ? false : true });
   }
 
   handleClick() {
-    this.props.addFavorite(this.props.entry.book_id + '_' + this.props.entry.chapter);
+    this.props.addFavorite(this.props.entry.book_id + '_' + this.props.entry.chapter_id);
   }
 
   render() {
     let tempId = this.props.entry.book.name + '_' + this.props.entry.chapter;
-    return (
-      <div className="entry-container column">
-        <button onClick={ this.handleClick.bind(this) }
-                key={tempId}
-                className="favorite">favorite</button>
-        <p className="entry-text">{this.props.entry.text}</p>
-        <span className="entry-author">{this.props.entry.author.name}</span>
-      </div>
-    );
+    let entryTextClasses = classnames('entry-text', { more: this.state.more });
+    return <div className="entry-container column">
+             <button onClick={ this.handleClick.bind(this) }
+                     key={tempId}
+                     className="favorite">favorite</button>
+             <p className={entryTextClasses}>{this.props.entry.text}</p>
+             <span className="entry-author">{this.props.entry.author.name}</span>
+             <a href="#" onClick={this.seeMore.bind(this)}>see more</a>
+           </div>
   }
 }
 
