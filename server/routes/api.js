e17d38b9a97c1744636837cf70e4cdb7b049ceb0
aequanimitas <htavenido@gmail.com>
2016-02-02 23:16:55 +0800

    move db related to models.js shell

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,29 +1,10 @@
 var express = require('express');
-var knex = require('knex')(require('../../knexfile').development);
-var Bookshelf = require('bookshelf')(knex);
+var models = require('../models');
 var router = express.Router();
 
 router.route('/entries')
   .get(function(req, res) {
-    var author = Bookshelf.Model.extend({ tableName: 'authors' });
-    var book = Bookshelf.Model.extend({ tableName: 'books' });
-    var chapter = Bookshelf.Model.extend({ tableName: 'chapter' });
-    var entry = Bookshelf.Model.extend({ 
-      tableName: 'entry',
-      author: function() {
-        return this.belongsTo(author);
-      },
-      book: function() {
-        return this.belongsTo(book);
-      },
-      chapter: function() {
-        return this.belongsTo(chapter);
-      }
-    });
-    var authors = Bookshelf.Collection.extend({ model: author });
-    var entries = Bookshelf.Collection.extend({ model: entry });
-
-    entries
+    models.entries
       .forge()
       .fetch({withRelated: ['author', 'book', 'chapter']})
       .then(function(collection) {
@@ -36,22 +17,7 @@ router.route('/entries')
 
 router.route('/entries/author/:id')
   .get(function(req, res) {
-    var author = Bookshelf.Model.extend({ tableName: 'authors' });
-    var book = Bookshelf.Model.extend({ tableName: 'books' });
-    var chapter = Bookshelf.Model.extend({ tableName: 'chapter' });
-    var entry = Bookshelf.Model.extend({ 
-      tableName: 'entry',
-      author: function() {
-        return this.belongsTo(author);
-      },
-      book: function() {
-        return this.belongsTo(book);
-      },
-      chapter: function() {
-        return this.belongsTo(chapter);
-      }
-    });
-    entry.query('where', 'author_id', '=', req.params.id)
+    models.entry.query('where', 'author_id', '=', req.params.id)
       .fetchAll({withRelated: ['author', 'book', 'chapter']})
       .then(function(entries) {
         res.json({error: false, data: entries.toJSON()});
@@ -61,5 +27,4 @@ router.route('/entries/author/:id')
       })
   });
 
-module.exports = router;
 module.exports = router;
