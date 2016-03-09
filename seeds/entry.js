1ad357c7179786260e4c7209d7eec4cdffe4f456
aequanimitas <htavenido@gmail.com>
2016-03-09 14:47:41 +0800

    seed musonius rufus

--- seeds/entry.js
+++ seeds/entry.js
@@ -14,6 +14,17 @@ exports.seed = function(knex, Promise) {
 	  return obj;
 	});
       return knex.insert(ench).into('entry');
+    }),
+    knex('book').select('bookId','authorIdFk').where('name','LIKE', '%Lucilius').then(function(d) {
+      var ench = entries.slice(5,10).map(function(e) {
+          var obj = {};
+	  obj.entryId = uuid.v4();
+	  obj.text = e.text;
+	  obj.authorIdFk = d[0].authorIdFk;
+	  obj.bookIdFk = d[0].bookId;
+	  return obj;
+	});
+      return knex.insert(ench).into('entry');
     })
   );
 }
