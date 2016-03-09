e3d2d7d67b7e23ecbbbf38d3b7ceb45832d5520e
aequanimitas <htavenido@gmail.com>
2016-03-09 14:43:44 +0800

    fix route and table referencing

--- server/routes/api/entries.js
+++ server/routes/api/entries.js
@@ -18,10 +18,10 @@ router.route('/').get(function(req, res) {
 });
 
 router.route('/author').get(function(req, res) {
-  models.knex('authors')
-    .join('entry', 'authors.id', 'entry.author_id')
-    .select('authors.name', 'entry.text')
-    .where('authors.name', '=', req.query.name)
+  models.knex('author')
+    .join('entry', 'author.authorId', 'entry.authorIdFk')
+    .select('author.name', 'entry.text')
+    .where('author.name', '=', req.query.name)
     .then(function(entries) {
       if (entries.length < 1) {
         console.log('reached');
