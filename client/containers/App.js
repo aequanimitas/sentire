060bdd846970e136406d71032231248bbac3a291
aequanimitas <htavenido@gmail.com>
2016-04-05 14:10:46 +0800

    just render first, no grouping

--- client/containers/App.js
+++ client/containers/App.js
@@ -4,24 +4,12 @@ import { bindActionCreators } from 'redux';
 import Entry from '../components/Entry';
 
 class App extends Component {
-  regroup(groupBy) {
-    let group = [];
-    for (var i = 0; i <= this.props.entries.length; i += groupBy) {
-      group.push(this.props.entries.slice(i, groupBy + i));
-    }
-    return group;
-  }
+
   render() {
-    const { entries } = this.props;
-    return <div className="row entry-row">
-      {this.regroup(4).map((group,i) => <div className="column" key={i}> 
-        {group.map(entry => {
-            let eKey = entry.author + entry.id + entry.chapter;
-            return <Entry entry={entry} 
-                   key={eKey} /> 
-           }
-        )}     
-      </div>)}
+    return <div>
+      {this.props.entries.map(entry => {
+        return <Entry entry={entry} key={entry.id} />
+      })}
     </div>
   }
 }
