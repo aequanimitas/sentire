30010d7367bc8df79fc2f599991843df0f2163c1
aequanimitas <htavenido@gmail.com>
2016-04-29 17:21:06 +0800

    add thunk to replicate for async middleware logic

--- test/helpers/reducers.js
+++ test/helpers/reducers.js
@@ -1,16 +1,23 @@
+const initialState = {
+  entries: { hidden: [], rendered: [], current: {} },
+  entryFetchCounter: { start: 0, limit: 30}
+}
 export function wrapReducer(spyOnMethods) {
   let eSpy = arguments[0]
-  return function reducers(state = [], action) {
+  return function reducers(state = initialState.entries, action) {
            switch(action.type) {
              case 'RECEIVED_ENTRIES':
-               return []
+               spyOnMethods(eSpy)
+               return {
+                 hidden: state.hidden,
+                 rendered: state.rendered,
+                 current: state.current
+               }
              case 'REHYDRATE_ENTRIES':
                return []
              case 'MOVE_CURRENT_ENTRY':
-               spyOnMethods(eSpy)
                return []
              case 'SET_CURRENT_ENTRY':
-               spyOnMethods(eSpy)
                return []
              default:
                return state;
--- test/middleware/entry.spec.js
+++ test/middleware/entry.spec.js
@@ -1,13 +1,27 @@
 import expect from 'expect';
 import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import { entry } from '../../client/middleware/entry'
 import { wrapReducer } from '../helpers/reducers'
 
 describe('Entry middleware', () => {
   it('should intercept RECEIVED_ENTRIES', () => {
     let spy = expect.createSpy(() => {})
-    let store = applyMiddleware(entry)(createStore)(wrapReducer(spy))
-    store.dispatch({type: 'RECEIVED_ENTRIES'})
-    expect(spy.calls.length).toEqual(2)
+    let store = applyMiddleware(thunkMiddleware, entry)(createStore)(wrapReducer(spy))
+    function receivedEntries(data, state) {
+      return { type: 'RECEIVED_ENTRIES',
+        entries: {
+          hidden: data.data,
+          rendered: state.entries.rendered,
+          current: state.entries.current
+        },
+        entryFetchCounter: state.entryFetchCounter
+      }
+    }
+    store.dispatch(
+      receivedEntries(
+        {data: [[],[],[]]}, {entries: {hidden: [], rendered:[], current:[]}, 
+        entryFetchCounter: {start: 0, limt: 30}}))
+    expect(spy.calls.length).toEqual(1)
   })
 })
