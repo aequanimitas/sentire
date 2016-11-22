185b39a9ca290fa09b3a7c4212e0209db6b7d2c8
aequanimitas <htavenido@gmail.com>
2016-11-22 10:19:37 +0800

    cleanup

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Sentire.PageControllerTest do
-  use Sentire.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
